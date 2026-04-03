afa5eb80c07c2c69d3e1436b225c949ef583db6b
Fathur <rizkyfathur326@gmail.com>
2026-04-03 16:25:31 +0700

    fix: shim SlowBuffer for legacy auth deps

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const bufferModule = require('buffer');
+
+if (!bufferModule.SlowBuffer) {
+  bufferModule.SlowBuffer = bufferModule.Buffer;
+}
+
 const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
