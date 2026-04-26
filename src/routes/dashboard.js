13602f2c950061ddff6b21d1963b3b1a44911263
orvin14 <13523017@std.stei.ac.id>
2026-04-26 10:04:22 +0700

    feat: add dashboard stats, charts, and recent

---
+++ src/routes/dashboard.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const dashboardController = require('../controllers/dashboard');
+const JWTValidation = require('../middlewares/auth');
+
+router.get('/stats', JWTValidation, dashboardController.getStats);
+router.get('/charts', JWTValidation, dashboardController.getCharts);
+router.get('/recent', JWTValidation, dashboardController.getRecent);
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -23,6 +23,7 @@ const PaymentsRouter = require('./payments');
 const FacultiesRouter = require('./faculties');
 const TallyWebhookRouter = require('./tallyWebhooks');
 const TallySubmissionsRouter = require('./tallySubmissions');
+const DashboardRouter = require('./dashboard');
 
 router.get('/', (req, res) => {
   res.json({
@@ -52,5 +53,6 @@ router.use('/payments', PaymentsRouter);
 router.use('/faculties', FacultiesRouter);
 router.use('/webhooks/tally', TallyWebhookRouter);
 router.use('/tally-submissions', TallySubmissionsRouter);
+router.use('/dashboard', DashboardRouter);
 
 module.exports = router;
